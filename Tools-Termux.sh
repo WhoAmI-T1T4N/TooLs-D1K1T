e94970739cd4a19f812aebe11fb1408b1ccf15b3
WhoAmI-T1T4N <92716161+WhoAmI-T1T4N@users.noreply.github.com>
2021-11-08 14:25:52 +0700

    Update Tools-Termux.sh

--- Tools-Termux.sh
+++ Tools-Termux.sh
@@ -23,7 +23,6 @@ echo $red "5). Spam Brutal"
 echo $red "6.). Keluar Aja"
 echo $black "++++++++++++++++++++++++++++++++++++++"
 read -p "Pilih Cepat Ngab :" santai
-
 if [[ $santai = "1" ]]
 then
 echo "TUNGGU NGAB, LAGI LOADING"
@@ -33,7 +32,6 @@ echo "TUNGGU NGAB, LAGI LOADING"
 else
 echo "TUNGGU NGAB, LAGI LOADING"
 fi
-
 if [[ $santai = "2" ]]
 then
    git clone https://github.com/Ranginang67/DarkFly-T>
@@ -41,14 +39,12 @@ then
        chmod +x *python2 install.py
              DarkFly
 fi
-
 if [[ $santai = "3" ]]
 then
     git clone https://github.com/grafov/hulk
       cd hulk
         python2 hulk.py
 fi
-
 if [[ $santai = "4" ]]
 then
      git clone https://github.com/cyweb/hammer
@@ -62,7 +58,6 @@ then
         cd Kang-Spam
           bash install.sh
 fi
-
 if [[ $santai = "6" ]]
 then
 figlet Selamat
